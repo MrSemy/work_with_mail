82738a5d0b83ada97f265fd6b8235cb454c4ae9d
Semyon <138588831+MrSemy@users.noreply.github.com>
2023-11-30 00:08:26 +0800

    Delete project directory

--- project/protect/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import IndexView
-
-
-urlpatterns = [
-    path('', IndexView.as_view()),
-    path('pages/', include('django.contrib.flatpages.urls')),
-    #path('', include('simpleapp.urls')),
-]
